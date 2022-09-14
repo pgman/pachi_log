87483c1ca81f261b18029bd61abf2705613e29d0
pgman <olj611@gmail.com>
2022-09-15 00:08:45 +0900

    Add files via upload

---
+++ pachi_log/shops.js
@@ -0,0 +1,42 @@
+const SHOPS = [
+	{
+		id: 'osakahalltengo',
+		name: '大阪ホール天五店',
+		info: '無し',
+	},
+	{
+		id: 'osakahalltenma',
+		name: '大阪ホール天満店',
+		info: '無し',
+	},		
+	{
+		id: 'daitoyohigashidori',
+		name: '大東洋東通店',
+		info: '無し',
+	},
+	{
+		id: 'daitoyoumeda',
+		name: '大東洋梅田店',
+		info: '無し',
+	},
+	{
+		id: 'kyobashikaikan',
+		name: '京橋会館',
+		info: '無し',
+	},
+	{
+		id: 'kyobashikaikan',
+		name: 'グランシャトー',
+		info: '無し',
+	},
+	{
+		id: 'n123osakahonten',
+		name: 'N123大阪本店',
+		info: '無し',
+	},
+	{
+		id: 'maruhanumeda',
+		name: 'マルハン梅田店',
+		info: '無し',
+	},		
+];
