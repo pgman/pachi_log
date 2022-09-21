36910aeb731435c5a45c2e18b96e3437e30316a9
maki <olj611@gmail.com>
2022-09-21 23:27:20 +0900

    test5

--- pachi_log/else-games.js
+++ pachi_log/else-games.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 他
 const ELSE_GAMES = [
 	
@@ -18,25 +17,4 @@ const ELSE_GAMES = [
 		info: 'メモを書き込んでください',
 		yuTime: false,
 	},
-=======
-// 他
-const ELSE_GAMES = [
-	
-	{
-		id: 'umi',
-		shortName: 'P 海系',
-		name: '海系',
-		border: 20.0,
-		info: 'メモを書き込んでください',
-		yuTime: false,
-	},
-	{
-		id: 'else',
-		shortName: 'P else',
-		name: '他',
-		border: 20.0,
-		info: 'メモを書き込んでください',
-		yuTime: false,
-	},
->>>>>>> 6c2378a94b72b393e98a15e2951827e25e9aa8fa
 ];
