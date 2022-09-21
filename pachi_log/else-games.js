6c3a52d24b30a1a965272db56833dc952851eb5b
maki <olj611@gmail.com>
2022-09-21 23:21:12 +0900

    test

---
+++ pachi_log/else-games.js
@@ -0,0 +1,20 @@
+// 他
+const ELSE_GAMES = [
+	
+	{
+		id: 'umi',
+		shortName: 'P 海系',
+		name: '海系',
+		border: 20.0,
+		info: 'メモを書き込んでください',
+		yuTime: false,
+	},
+	{
+		id: 'else',
+		shortName: 'P else',
+		name: '他',
+		border: 20.0,
+		info: 'メモを書き込んでください',
+		yuTime: false,
+	},
+];
---
+++ pachi_log/shops.js
@@ -0,0 +1,50 @@
+const SHOPS = [
+	{
+		id: 'osakahalltengo',
+		name: '大阪ホール天五店',
+		shortName: '天五店',
+		info: '無し',
+	},
+	{
+		id: 'osakahalltenma',
+		name: '大阪ホール天満店',
+		shortName: '天満店',
+		info: '無し',
+	},		
+	{
+		id: 'daitoyohigashidori',
+		name: '大東洋東通店',
+		shortName: '東通店',
+		info: '無し',
+	},
+	{
+		id: 'daitoyoumeda',
+		name: '大東洋梅田店',
+		shortName: '梅田店',
+		info: '無し',
+	},
+	{
+		id: 'kyobashikaikan',
+		name: '京橋会館',
+		shortName: '京橋ボッタ',
+		info: '無し',
+	},
+	{
+		id: 'kyobashikaikan',
+		name: 'グランシャトー',
+		shortName: 'グラン',
+		info: '無し',
+	},
+	{
+		id: 'n123osakahonten',
+		name: 'N123大阪本店',
+		shortName: '123本店',
+		info: '無し',
+	},
+	{
+		id: 'maruhanumeda',
+		name: 'マルハン梅田店',
+		shortName: 'マル梅田',
+		info: '無し',
+	},		
+];
